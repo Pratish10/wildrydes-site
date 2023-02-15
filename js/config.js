88abff761494effc1f4bd0c3f69a25827ddfcd9b
Pratish Ninawe <pratish.1006@gmail.com>
2023-02-15 12:33:45 +0530

    update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "ap-south-1_GKvOfdI8t", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "7mhp0kkulcicb3q5nih9o28lsc", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-south-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
